4d4135df7058e9039089e7e9075af7efc604c4d6
Tu Rodrigo <vallejos.rodrigo@outlook.com>
2024-07-30 18:52:10 -0300

    Implement 'SplashActivity' class

---
+++ app/src/main/java/com/softchar/contadordardos/ui/game/splash/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.softchar.contadordardos.ui.game.splash
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.softchar.contadordardos.R
+import com.softchar.contadordardos.ui.game.menu.MenuActivity
+
+class SplashActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MenuActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3500)
+    }
+
+}
